74c6c1f3f54fca600898830cb7ebc805ce7643ad
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2022-05-11 19:20:17 +0200

    Update

---
+++ Tema1/src/Ejercicio11.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ejercicio11 {
+
+	public static void main(String[] args) {
+		/*
+		 * Un economista te ha encargado un programa para realizar cálculos con el IVA. La aplicación debe 
+		 * solicitar la base imponible y el IVA que se debe aplicar. Muestra en pantalla el importe 
+		 * correspondiente al IVA y al total.
+		 */
+		
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("Ingrese el valor de la base imponible:");
+		double base_imponible=sc.nextDouble();
+		System.out.println("Ingrese el valor del IVQ que se debe aplicar: ");
+		double iva=sc.nextDouble();
+		
+		double total=base_imponible+iva;
+		
+		System.out.println(total);
+		
+		
+		
+
+		
+		
+	}
+
+}
