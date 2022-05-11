ef0d915472d122d8c828a2f960499969f71ff3fb
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2022-05-11 20:26:30 +0200

    Completo

--- Tema1/src/Ejercicio11.java
+++ Tema1/src/Ejercicio11.java
@@ -9,23 +9,31 @@ public class Ejercicio11 {
 		 * correspondiente al IVA y al total.
 		 */
 		
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+					
+		System.out.println("Ingrese la base imponible del producto: ");
+					
+		double BaseIm= sc.nextDouble();
+					
+		System.out.println("Ingrese el IVA que desea aplicar: ");
+					
+		double iva= sc.nextDouble();
+					
+		double totiva;
+					
+		totiva=(BaseIm*iva)/100;
+					
+		System.out.println("El importe correspondientes al iva es:" +totiva);
+					
+		double total;
+					
+		total= (BaseIm + totiva);
+					
+		System.out.println("El importe total del producto es : " + total);
+					
+		sc.close();
+				
+		}		
 		
-		System.out.println("Ingrese el valor de la base imponible:");
-		double base_imponible=sc.nextDouble();
-		System.out.println("Ingrese el valor del IVQ que se debe aplicar: ");
-		double iva=sc.nextDouble();
-		
-		double total=base_imponible+iva;
-		
-		System.out.println("El total es: "+total);
-
-		
-		
-		
-
-		
-		
-	}
-
 }
+
